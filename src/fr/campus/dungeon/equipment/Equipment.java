a66705b2f8a442e95f57c59e6ffa6dd57d4c0044
v-swan <swan.vuillet@hotmail.com>
2026-07-21 12:01:46 +0200

    Première itération de la class Equipment

--- src/fr/campus/dungeon/equipment/Equipment.java
+++ src/fr/campus/dungeon/equipment/Equipment.java
@@ -1,4 +1,22 @@
 package fr.campus.dungeon.equipment;
 
 public class Equipment {
+    private String name;
+
+
+    public Equipment(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Equipment{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
