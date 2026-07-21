3948da2db89658cd0848c8ef5da48b3005cf23e3
v-swan <swan.vuillet@hotmail.com>
2026-07-21 14:12:41 +0200

    Create OffensiveEquipment class with attack bonus management

--- src/fr/campus/dungeon/equipment/OffensiveEquipment.java
+++ src/fr/campus/dungeon/equipment/OffensiveEquipment.java
@@ -1,4 +1,23 @@
 package fr.campus.dungeon.equipment;
 
-public class OffensiveEquipment {
+
+
+public class OffensiveEquipment extends Equipment {
+    private int bonusAttack;
+
+    public OffensiveEquipment(String name, int bonusAttack){
+        super(name);
+        this.bonusAttack = bonusAttack;
+
+    }
+
+    public int getBonusAttack() {
+        return bonusAttack;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                " +" + bonusAttack + " attaque";
+    }
 }
