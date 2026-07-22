424e6b30c18d6584597286dc37a1790a010805c1
v-swan <swan.vuillet@hotmail.com>
2026-07-22 12:09:43 +0200

    feat: create console main menu with user input

--- src/fr/campus/dungeon/menu/Menu.java
+++ src/fr/campus/dungeon/menu/Menu.java
@@ -1,4 +1,17 @@
 package fr.campus.dungeon.menu;
+import java.util.Scanner;
 
 public class Menu  {
+    private Scanner scanner = new Scanner(System.in);
+
+    public int mainMenu () {
+        System.out.println("1 - New Game");
+        System.out.println("2 - Quitter");
+        int userChoice = scanner.nextInt();
+        return userChoice;
+
+    }
+
+
 }
+
