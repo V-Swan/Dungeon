f08f36734fda7e73a4f7cff3dbc7657f3f8664e4
v-swan <swan.vuillet@hotmail.com>
2026-07-22 09:13:22 +0200

    Add DefensiveEquipment class with DamageReduction for Equipment

--- src/fr/campus/dungeon/equipment/DefensiveEquipment.java
+++ src/fr/campus/dungeon/equipment/DefensiveEquipment.java
@@ -1,4 +1,22 @@
 package fr.campus.dungeon.equipment;
 
-public class DefensiveEquipment {
-}
+public class DefensiveEquipment extends Equipment{
+
+        private int damageReduction;
+
+        public DefensiveEquipment(String name, int damageReduction){
+            super(name);
+            this.damageReduction = damageReduction;
+
+        }
+
+        public int getDamageReduction() {
+            return damageReduction;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() +
+                    " -" + damageReduction + " dégât";
+        }
+    }
