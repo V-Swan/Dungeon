8ae1a576701b17afb244b1584174ba92702e5d8a
v-swan <swan.vuillet@hotmail.com>
2026-07-22 09:27:53 +0200

    Test equipment creation and display

--- src/fr/campus/dungeon/Main.java
+++ src/fr/campus/dungeon/Main.java
@@ -1,11 +1,21 @@
 package fr.campus.dungeon;
 
-import fr.campus.dungeon.game.Game;
+import fr.campus.dungeon.equipment.Consumable;
+import fr.campus.dungeon.equipment.DefensiveEquipment;
+import fr.campus.dungeon.equipment.OffensiveEquipment;
 
 public class Main {
 
     public static void main(String[] args) {
-        Game game = new Game();
-        game.start();
+       // Game game = new Game();
+       // game.start();
+
+
+        OffensiveEquipment steelSword = new OffensiveEquipment("Steel sword", 7);
+        DefensiveEquipment shield = new DefensiveEquipment("Steel Shield", 3);
+        Consumable highPotion = new Consumable("High potion", 8);
+        System.out.println(steelSword);
+        System.out.println(shield);
+        System.out.println(highPotion);
     }
 }
--- src/fr/campus/dungeon/equipment/Consumable.java
+++ src/fr/campus/dungeon/equipment/Consumable.java
@@ -17,6 +17,6 @@ public class Consumable extends Equipment{
     @Override
     public String toString() {
         return super.toString() +
-                " + " + healingValue + " point de vie";
+                " +" + healingValue + " point de vie";
     }
 }
