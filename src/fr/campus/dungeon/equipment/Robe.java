3411f8b4ef5f235b039353872ee63ea6f8a8264f
v-swan <swan.vuillet@hotmail.com>
2026-07-23 11:47:47 +0200

    Feat: Creation of a robe class for the defensive equipment of the Wizard

---
+++ src/fr/campus/dungeon/equipment/Robe.java
@@ -0,0 +1,13 @@
+package fr.campus.dungeon.equipment;
+
+public class Robe extends DefensiveEquipment {
+
+    public Robe(
+            String name,
+    int damageReduction)
+    {
+        super(
+                name,
+                damageReduction);
+    }
+}
