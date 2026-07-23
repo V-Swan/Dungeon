7020199a597204ed62aa10fb91972f01948e21a8
v-swan <swan.vuillet@hotmail.com>
2026-07-23 11:48:50 +0200

    Feat: Added the Character Creation/Naming

--- src/fr/campus/dungeon/game/Game.java
+++ src/fr/campus/dungeon/game/Game.java
@@ -1,17 +1,42 @@
 package fr.campus.dungeon.game;
+import fr.campus.dungeon.equipment.*;
 import fr.campus.dungeon.menu.Menu;
+import fr.campus.dungeon.character.PlayerCharacter;
+import fr.campus.dungeon.character.Warrior;
+import fr.campus.dungeon.character.Wizard;
 
 public class Game {
     private final Menu MENU = new Menu();
+    private PlayerCharacter character;
 
     public void start() {
         String choice = MENU.mainMenu();
         if (choice.equals("1")) {
-            // Démarrer le jeu
-            System.out.println("Lancer le jeu...");
+                startGame();
+
         } else if (choice.equals("2")) {
             // Quitter
             System.out.println("Au revoir !");
         }
     }
-}
+    private void startGame() {
+        System.out.println("=== Création du personnage ===");
+        character = createCharacter();
+        System.out.println("\n" + character);
+    }
+
+    private PlayerCharacter createCharacter() {
+        String characterClass = MENU.characterClassMenu();
+        String playerName = MENU.getPlayerName();
+
+        return switch (characterClass) {
+            case "Warrior" -> new Warrior(playerName, 10, 5,
+                    new Weapon("Épée", 5),
+                    new Shield("Bouclier en fer", 2));
+            case "Wizard" -> new Wizard(playerName, 6, 8,
+                    new Spell("Boule de feu", 7),
+                    new Robe ( "Robe en tissue", 1 ));
+            default -> throw new IllegalArgumentException("Classe inconnue");
+        };
+    }
+        }
